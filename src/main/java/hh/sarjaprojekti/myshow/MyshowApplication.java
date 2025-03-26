c6870fcc30fa54312a31bf21c55ac101122a2db1
ArjonD <bhm527@myy.haaga-helia.fi>
2025-03-26 21:16:20 +0200

    First commit

---
+++ src/main/java/hh/sarjaprojekti/myshow/MyshowApplication.java
@@ -0,0 +1,13 @@
+package hh.sarjaprojekti.myshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyshowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyshowApplication.class, args);
+	}
+
+}
